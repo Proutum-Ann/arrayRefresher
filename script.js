5d0d0eb7e333bd0a3e22a0dbe78fc13baa2acf30
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-10-02 20:54:29 +0000

    finished making list

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const toons = ['Dandicus Dancifer', 'Pebble Dancifer Jr.', 'Astro Novalite', 'Shelly Fossilian', 'Sprout Seedly', 'Vee Version 1', 'Shrimpo', '']
+const toons = ['Dandicus Dancifer', 'Pebble Dancifer Jr.', 'Astro Novalite', 'Shelly Fossilian', 'Sprout Seedly', 'Vee Version 1', 'Dyle Timesly', 'Shrimpo', 'Poppy', 'Boxten', 'Brighteny', 'Glisten', 'Razzle & Dazzle', 'Scraps', 'Goob', 'Bassie Bloomington', 'Bobette Carolynne', 'Tisha', 'Brusha', 'Blot', 'Looey', 'Yatta', 'Coal', 'Ginger', 'Rudie', 'Cocoa', 'Flyte', 'Flutter', 'Eggson', 'Finn', 'Connie', 'Soulvester', 'Ribecca', 'Cosmo', 'Gigi', 'Rodger', 'Teagan', 'Toodles']
+
+const toonList = document.getElementById("output")
+
+toons.forEach(function(t) {
+        const toonLine = document.createElement("li")
+        toonLine.style.listStyleType = 'none'
+
+        toonLine.innerText = t
+
+        toonList.appendChild(toonLine)
+    })
