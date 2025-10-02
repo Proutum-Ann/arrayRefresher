6bd12683a3e977d77318bd88d1ef0d849b5aabbd
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-10-02 20:54:49 +0000

    list now sorted alphabetically

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const toons = ['Dandicus Dancifer', 'Pebble Dancifer Jr.', 'Astro Novalite', 'Shelly Fossilian', 'Sprout Seedly', 'Vee Version 1', 'Dyle Timesly', 'Shrimpo', 'Poppy', 'Boxten', 'Brighteny', 'Glisten', 'Razzle & Dazzle', 'Scraps', 'Goob', 'Bassie Bloomington', 'Bobette Carolynne', 'Tisha', 'Brusha', 'Blot', 'Looey', 'Yatta', 'Coal', 'Ginger', 'Rudie', 'Cocoa', 'Flyte', 'Flutter', 'Eggson', 'Finn', 'Connie', 'Soulvester', 'Ribecca', 'Cosmo', 'Gigi', 'Rodger', 'Teagan', 'Toodles']
 
+toons.sort();
+
 const toonList = document.getElementById("output")
 
 toons.forEach(function(t) {
